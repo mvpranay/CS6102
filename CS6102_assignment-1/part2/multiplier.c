acb8cc5fc819e61013845d3556757d7c141a9650
mv.pranay <mvpranavy@gmail.com>
2026-02-19 02:22:32 +0530

    tried 2nd version of optimized code

--- CS6102_assignment-1/part2/multiplier.c
+++ CS6102_assignment-1/part2/multiplier.c
@@ -1,4 +1,5 @@
 #include "multiplier.h"
+#include "stdio.h"
 
 /**
  * GF(16) multiplication
@@ -7,5 +8,32 @@
 unsigned char mul_f(unsigned char a, unsigned char b)
 {
     // Write your code here
-    return 0;
+    unsigned char p = 0;
+    
+    // Unrolled loop for 4 bits (No branches)
+    // Bit 0
+    p = (b & 1) ? a : 0;
+    a = (a << 1) ^ ((a & 0x8) ? 0x13 : 0);
+    
+    // Bit 1
+    p ^= (b & 2) ? a : 0;
+    a = (a << 1) ^ ((a & 0x8) ? 0x13 : 0);
+    
+    // Bit 2
+    p ^= (b & 4) ? a : 0;
+    a = (a << 1) ^ ((a & 0x8) ? 0x13 : 0);
+    
+    // Bit 3
+    p ^= (b & 8) ? a : 0;
+    return p & 0xF;
 }
+
+// int main() {
+//     for (int i = 0; i < 16; i++) {
+//         for (int j = 0; j < 16; j++) {
+//             // %x prints lowercase hex
+//             printf("RESULT: %x %x = %x\n", i, j, mul_f(i, j));
+//         }
+//     }
+//     return 0;
+// }

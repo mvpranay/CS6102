e086c111d471ee251cc408cd9447d54876293f65
mv.pranay <mvpranavy@gmail.com>
2026-03-03 23:30:36 +0530

    part2 : did log, antilog tables

--- CS6102_assignment-1/part2/multiplier.c
+++ CS6102_assignment-1/part2/multiplier.c
@@ -5,19 +5,16 @@
  * GF(16) multiplication
  * Mod Polynomial: x^4 + x + 1 (0x13)
  */
+static char exp_table[15] = {1, 2, 4, 8, 3, 6, 12, 11, 5, 10, 7, 14, 15, 13, 9};
+static char log_table[16] = {0, 0, 1, 4, 2, 8, 5, 10, 3, 14, 9, 7, 6, 13, 11, 12};
+
 unsigned char mul_f(unsigned char a, unsigned char b)
 {
-    
-    unsigned char r0_2 = ((a & 2) * b) ^ ((a & 1) * b) ^ ((a & 4) * b) ^ ((a & 8) * b);
-    return (r0_2 ^ (r0_2 >> 4 & 0xf) ^ (r0_2 & 0xf0) >> 3) & 0xf;
+    if (a == 0 | b == 0) {
+        return 0;
+    }
+    char log_a = log_table[a];
+    char log_b = log_table[b];
+    char log_result = (log_a + log_b) % 15;
+    return exp_table[log_result];
 }
-
-// int main() {
-//     for (int i = 0; i < 16; i++) {
-//         for (int j = 0; j < 16; j++) {
-//             // %x prints lowercase hex
-//             printf("RESULT: %x %x = %x\n", i, j, mul_f(i, j));
-//         }
-//     }
-//     return 0;
-// }
